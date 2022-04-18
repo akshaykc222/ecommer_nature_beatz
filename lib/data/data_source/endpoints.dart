f6812175a7d6a9dec881ceeb4736638843948521
askahay <akshaykc222@gmail.com>
2022-04-18 08:44:25 +0530

    added registration

--- lib/data/data_source/endpoints.dart
+++ lib/data/data_source/endpoints.dart
@@ -1,3 +1,7 @@
-class EndPoints {
-  // static const registerApi = Uri.parse("$base")
+class ApiEndPoints {
+  static const baseUrl = "https://nature-beatz.herokuapp.com/";
+  static const version = "/api/v1/";
+  static const registerApi = "$baseUrl$version/register/";
+  static const loginApi = "$baseUrl$version/login/";
+  static const categories = "$baseUrl$version/categories/";
 }
--- lib/data/data_source/remote_repositary.dart
+++ lib/data/data_source/remote_repositary.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:shop_app/data/data_source/endpoints.dart';
 import 'package:shop_app/data/models/register_model.dart';
 
 import '../exceotion.dart';
@@ -9,16 +10,15 @@ abstract class RemoteDataSource {
   Future<RegisterModel> register(RegisterModel model);
 }
 
-class RemoteDataSourceImpl implements RemoteDataSource {
+class RemoteDataSourceImpl implements RemoteDataSource, ApiEndPoints {
   final http.Client client;
   RemoteDataSourceImpl({required this.client});
 
   @override
   Future<RegisterModel> register(RegisterModel model) async {
-    final response =
-        await client.get(Uri.parse(Urls.currentWeatherByName(cityName)));
+    final response = await client.get(Uri.parse(ApiEndPoints.registerApi));
 
-    if (response.statusCode == 200) {
+    if (response.statusCode == 201) {
       return RegisterModel.fromJson(json.decode(response.body));
     } else {
       throw ServerException();
